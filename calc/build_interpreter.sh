bdb2eb88cf97d93c95743f07c3d848e49c5689f6
Iten-No-404 <iten_emad@yahoo.com>
2023-05-23 14:57:10 +0300

    Delete Extra Files

--- calc/build_interpreter.sh
+++
@@ -1,5 +0,0 @@
-set -xe
-
-bison -d calc.y									  # create y.tab.h, y.tab.c
-lex calc.l                        # create lex.yy.c
-gcc lex.yy.c calc.tab.c calc_interpreter.c -o calc_interperter     # compile/link
