1330e7c1f02265f79ea3c97c5582f1d1c25b6fda
Tarek Yasser <tarekspeed7@gmail.com>
2023-05-28 21:46:10 +0300

    Add debug option to Bison in CMakeLists, update build.sh.

--- build.sh
+++ build.sh
@@ -1,49 +1,37 @@
+set -xe
+
 debug=$2
 
 help() {
-	echo "USAGE: $0 {interpreter|graph|compiler|generate_code} [--debug]"
+	# $0 is the script name
+	echo "USAGE: $0 {(i)nterpreter|(c)ompiler|clean} [--debug]"
 }
 
-clean() {
-	rm y.tab.c parser.h lex.yy.c shbl_interpreter shbl_compiler shbl_graph
-}
-
-generate_code() {
-	set -xe
-	bison -y -d $debug cl.y            # create parser.h, y.tab.c
-	lex cl.l                           # create lex.yy.c
-}
-
-build() {
-    # Make sure no backslash is followed by a space!
-	clang++-16 \
-        -ggdb -stdlib=libc++ -std=c++17 \
-        -Wno-write-strings -Wno-format-security \
-        semantic_utils.cpp setup_scopes.cpp value_operators.cpp node_constructors.cpp cls.cpp lex.yy.c y.tab.c \
-        $1 -o $2
+cmake_prep() {
+	mkdir -p build
+	cd build
+	cmake ../ -DBISON_DEBUG:STRING="$debug"
 }
 
 interpreter() {
-	generate_code
-	build cli.cpp shbl_interpreter 
+	cmake_prep
+	make -j$(nproc) SHEBLANG_interpreter 
 }
 
-graph() {
-	generate_code
-	build clg.c shbl_graph 
+compiler() { 
+	cmake_prep
+	make -j$(nproc) SHEBLANG_compiler 
 }
 
-compiler() {
-	generate_code
-	build clc.cpp shbl_compiler 
+clean() {
+	cd build
+	make clean
 }
 
 case $1 in 
 	# pattern) command ;;
 	interpreter|i) interpreter ;;
 	compiler|c) compiler ;;
-	graph) graph ;;
-	generate_code) generate_code ;;
-	clean) clean;;
+	clean) clean ;;
 	*) help ;;
 esac 
